cb6250670c57dee402b1189c90efbe45223d86c6
Anders <idris.maps@gmx.ch>
2020-11-13 22:00:30 +0100

    Add maki icons

--- packages/icons/index.js
+++ packages/icons/index.js
@@ -1,7 +1,10 @@
 import _Feather from './feather'
+import _Maki from './maki'
 
 export const Feather = Feather
+export const Maki = _Maki
 
 export default {
   Feather,
+  Maki,
 }
---
+++ packages/icons/maki.sh
@@ -0,0 +1,9 @@
+rm -rf temp/maki
+rm -rf maki
+
+mkdir -p temp/maki
+mkdir maki
+
+npx degit https://github.com/mapbox/maki/icons temp/maki
+
+#node maki.build.js
