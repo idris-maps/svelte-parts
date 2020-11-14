3ff3ed5951799db8ed75d229d98c35a6d4cfadc4
Anders <idris.maps@gmx.ch>
2020-11-14 20:24:27 +0100

    Add css and readme to editor

--- packages/editor/Editor.svelte
+++ packages/editor/Editor.svelte
@@ -1,28 +1,28 @@
 <script lang="ts">
-	import Editor from './codemirror/CodeMirror.svelte'
-	import 'codemirror/mode/gfm/gfm'
+  import Editor from './codemirror/CodeMirror.svelte'
+  import 'codemirror/mode/gfm/gfm'
 
   export let theme = 'default'
   export let onChange = undefined
   export let initialValue = ''
 
-	const config = {
-		lineNumbers: true,
-		lineWrapping: true,
-		theme,
-		mode: {
+  const config = {
+    lineNumbers: true,
+    lineWrapping: true,
+    theme,
+    mode: {
       name: 'gfm',
       highlightFormatting: true,
-		},
-	}
+    },
+  }
 
-	const accessEditor = editor => {
+  const accessEditor = editor => {
     editor.setSize('100%', '100%')
     editor.on('change', e => {
       if (onChange) { onChange(e.getValue()) }
     })
     editor.setValue(initialValue)
-	}
+  }
 </script>
 
 
