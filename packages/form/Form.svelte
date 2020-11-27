48b03536e7c18d0c56f33b8de3ce6def1de28a20
Anders <idris.maps@gmx.ch>
2020-11-27 22:40:07 +0100

    Formatting

--- packages/form/Form.svelte
+++ packages/form/Form.svelte
@@ -1,15 +1,15 @@
 <script>
-	import getInitialData from './getInitialData'
+  import getInitialData from './getInitialData'
   import RenderField from './field/index.svelte'
 
-	export let fields = []
+  export let fields = []
   export let onSubmit
-	export let submitButton
-	export let errorMessage
+  export let submitButton
+  export let errorMessage
 
-	let data = getInitialData(fields)
-	const setData = (key, value) => {
-		data = { ...data, [key]: value }
+  let data = getInitialData(fields)
+  const setData = (key, value) => {
+    data = { ...data, [key]: value }
   }
 
   const onNativeSubmit = e => {
@@ -18,21 +18,20 @@
       onSubmit(data)
     }
   }
-
 </script>
 
 <form on:submit={onNativeSubmit}>
-	{#each fields as field}
-		<RenderField field={field} onChange={setData} />
-	{/each}
-	{#if errorMessage}
-		<p class="error-message">{ errorMessage }</p>
-	{/if}
-	<div class="field submit-field">
-		{#if submitButton}
-			<input type="submit" value={submitButton} />
-		{:else}
-			<input type="submit" />
-		{/if}
-	</div>
+  {#each fields as field}
+    <RenderField field={field} onChange={setData} />
+  {/each}
+  {#if errorMessage}
+    <p class="error-message">{ errorMessage }</p>
+  {/if}
+  <div class="field submit-field">
+    {#if submitButton}
+      <input type="submit" value={submitButton} />
+    {:else}
+      <input type="submit" />
+    {/if}
+  </div>
 </form>
