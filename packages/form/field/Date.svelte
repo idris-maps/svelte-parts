3681e9c3865a041b0b9d0b3d3182064e56748d90
Anders <idris.maps@gmx.ch>
2020-11-27 21:16:07 +0100

    Add padding to date input

--- packages/form/field/Date.svelte
+++ packages/form/field/Date.svelte
@@ -1,10 +1,12 @@
 <script>
   export let field
   export let onChange
+
+  let pristine = true
   let value = field.value
 </script>
 
-<div class="field date-field">
+<div class="field date-field {pristine ? 'pristine' : ''}">
   <label for={field.property}>{ field.label || field.property }</label>
   <input
     type="date"
@@ -14,6 +16,8 @@
     min={field.min}
     max={field.max}
     required={!field.notRequired}
+    on:blur={() => pristine = false}
+    on:keyup={() => pristine = false}
     on:change={e => {
       value = e.currentTarget.value
       onChange(field.property, value)
